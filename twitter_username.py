ad2ebc6cd996f28b64f43c67311857ec5fd7f070
DBerrones <53327546+danielberrones@users.noreply.github.com>
2020-04-26 15:13:07 -0500

    Updated File
    Hey, just saw your code on Reddit.  I updated the way you import tkinter.  It is generally not advised to use a wildcard import.

--- twitter_username.py
+++ twitter_username.py
@@ -1,15 +1,15 @@
 
 from twitter_scraper import Profile
-from tkinter import *
+impor tkinter as tk 
 
-root = Tk()
+root = tk.Tk()
 root.title("Twitter Handle")
 root.iconbitmap("twitterlogo.ico")
 
-instructions = Label(root, text = "Enter your desired handle below: ")
+instructions = tk.Label(root, text = "Enter your desired handle below: ")
 instructions.grid(row = 0, column = 1,)
 
-handle = Entry(root)
+handle = tk.Entry(root)
 handle.grid(row = 1, column = 1)
 
 #for handle
@@ -27,13 +27,13 @@ def handle_search(username_):
 		background = "green"
 		padding = 65
 
-	display = Label(root, text = a, bg = background, padx = padding)
+	display = tk.Label(root, text = a, bg = background, padx = padding)
 	display.grid(row = 4, column = 1)
 
-submit_button = Button(root, text = "Submit", command = lambda: handle_search(handle.get()))
+submit_button = tk.Button(root, text = "Submit", command = lambda: handle_search(handle.get()))
 submit_button.grid(row = 2, column = 1)
 
-avaliability = Label(root, text = "Availiablity: ")
+avaliability = tk.Label(root, text = "Availiablity: ")
 avaliability.grid(row = 3, column = 1)
 
 root.mainloop()
